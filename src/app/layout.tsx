8095a6e187265d66f53d401dda8d753e8fa8181b
Faris Rayhan <frayhan94@gmail.com>
2025-03-16 16:51:04 +0700

    feat: remove unnecessary import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { useThemeStore } from "@/store/themeStore";
 
@@ -14,6 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={darkMode ? "dark" : ""}>
       <body className={`antialiased`}>
+
         <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import SearchBar from "@/components/SearchBar";
 import RepoList from "@/components/RepoList";
-import { NextSeo } from "next-seo";
 import { useSearchStore } from "@/store/searchStore";
 import { useEffect } from "react";
 
@@ -30,17 +29,17 @@ export default function Home() {
     };
   }, [showModalRepoList]);
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start py-10 px-4">
-      <h1 className={"text-2xl sm:text-3xl text-gray-900 mb-2 text-centered"}>
-        Github Repository Explorer
-      </h1>
-      <span className={"text-gray-600"}>
-        Search for GitHub users and explore their public repositories easily.
-      </span>
-      <span className={"mb-6 text-gray-600"}>Created by Faris Rayhan.</span>
-      <SearchBar />
-      {/*modal repository list*/}
-      {showModalRepoList && <RepoList />}
-    </div>
+      <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-start py-10 px-4">
+        <h1 className={"text-2xl sm:text-3xl text-gray-900 mb-2 text-centered"}>
+          Github Repository Explorer
+        </h1>
+        <span className={"text-gray-600"}>
+          Search for GitHub users and explore their public repositories easily.
+        </span>
+        <span className={"mb-6 text-gray-600"}>Created by Faris Rayhan.</span>
+        <SearchBar />
+        {/*modal repository list*/}
+        {showModalRepoList && <RepoList />}
+      </div>
   );
 }
