330edf3140a9db5fe571eb9fe11c1686bb2df6a3
Faris Rayhan <frayhan94@gmail.com>
2025-03-16 14:07:54 +0700

    feat: add pagination for repo

--- src/hooks/useGithubRepos.ts
+++ src/hooks/useGithubRepos.ts
@@ -1,9 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery,UseQueryResult } from "@tanstack/react-query";
 import axios from "axios";
+import { Repo } from "@/types";
 const GITHUB_ACCESS_TOKEN = process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN;
 
-const fetchGithubRepos = async (username: string) => {
+const fetchGithubRepos = async (
+  username: string,
+  page: number,
+  per_page: number
+): Promise<Repo> => {
   const { data } = await axios.get(`https://api.github.com/users/${username}/repos`, {
+    params: { page, per_page },
     headers: {
       Authorization: `Bearer ${GITHUB_ACCESS_TOKEN}`,
       Accept: "application/vnd.github.v3+json",
@@ -12,10 +18,10 @@ const fetchGithubRepos = async (username: string) => {
   return data;
 };
 
-export function useGithubRepos(username: string) {
+export function useGithubRepos(username: string, page: number, per_page = 10): UseQueryResult<Repo[]> {
   return useQuery({
     queryKey: ["githubRepos", username],
-    queryFn: () => fetchGithubRepos(username),
+    queryFn: () => fetchGithubRepos(username, page, per_page),
     retry: 3,
     enabled: !!username,
     staleTime: 1000 * 60 * 5, // 5 minutes
