d56404aa56a66273669592ff18abc00947c9d338
Faris Rayhan <frayhan94@gmail.com>
2025-03-16 17:06:24 +0700

    feat:  remove unnecessary file

--- src/components/DarkModeToggle.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { useThemeStore } from "@/store/themeStore";
-export default function DarkModeToggle() {
-  const { darkMode, toggleDarkMode } = useThemeStore();
-  return <button onClick={toggleDarkMode}>{darkMode ? "Dark" : "Light"} Mode</button>;
-}
